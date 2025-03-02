8848031e4a6ec37fdd9a8ac59b6d3e416d6ecba6
Nitish Kumar <103118427+sdnitish@users.noreply.github.com>
2025-03-02 15:59:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        data-testid="name"
+      />
+      {
+        name ? (
+          <p data-testid="greet-text">Hello, {name} !</p>
+        ) : null
+      }
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
